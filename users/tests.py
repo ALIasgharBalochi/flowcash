47054f21a76f8aa9dd588959e4149fcc238b875a
ali balochi <alibalochi1910@gmail.com>
2025-08-24 16:14:45 +0330

    add unit test for change password

--- users/tests.py
+++ users/tests.py
@@ -78,3 +78,33 @@ class ProfileUpdateTest(APITestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["first_name"], "NewName")
+
+class ChangePasswordTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@example.com",
+            password="password123",
+            first_name="testname",
+            last_name="testFname"
+        )
+
+        response = self.client.post("/account/login/token/", {
+            "email": "test@example.com",
+            "password": "password123"
+        }, format="json")
+
+        self.token = response.data["access"]
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
+
+    def test_change_password(self):
+        url = reverse('re_password')
+        data = {
+            "old_password": "password123",
+            "new_password": "passwordNew"
+        }
+
+        response = self.client.post(url,data,format="json")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["message"],"Password changed successfully.")
