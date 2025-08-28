126deb844276f38ae80707680cc4369e794c60ee
ali balochi <alibalochi1910@gmail.com>
2025-08-29 00:47:38 +0330

    add unit test for create category

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class CategoryTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test@example.com",
+            password="password123",
+            first_name="testname",
+            last_name="testFname"
+        )
+
+        response = self.client.post("/account/login/token/", {
+            "email": "test@example.com",
+            "password": "password123"
+        }, format="json")
+
+        self.token = response.data["access"]
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
+
+    def test_create_category(self):
+
+        response = self.client.post('/expenses/category/',{
+            "name":"fun"
+        })
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data["name"],"fun")
+
