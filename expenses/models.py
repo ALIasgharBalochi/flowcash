cd39646c85c980adae5bebee7e40e204df220a73
ali balochi <alibalochi1910@gmail.com>
2025-09-02 22:05:25 +0330

    create recurring model

--- expenses/models.py
+++ expenses/models.py
@@ -6,6 +6,27 @@ class Category(models.Model):
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,blank=True,null=True,related_name='category')
     is_default = models.BooleanField(default=False)
 
+class RecurringExpense(models.Model):
+    FREQUENCY_CHOICES = [
+        ('daily', 'Daily'),
+        ('weekly','Weekly'),
+        ('monthly','Monthly'),
+        ('yearly', 'Yearly')
+    ]
+
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20,decimal_places=2)
+    description = models.CharField(max_length=255)
+    frequency = models.CharField(max_length=20,choices=FREQUENCY_CHOICES)
+    anchor_date = models.DateField()
+    next_run_at = models.DateField
+    active = models.BooleanField(default=True)
+    end_date = models.DateField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Expense(models.Model):
     amount = models.DecimalField(max_digits=20,decimal_places=2)
     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True,related_name='expense')
@@ -13,6 +34,7 @@ class Expense(models.Model):
     description = models.CharField(max_length=255,blank=True,null=True)
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='expense')
     created_at = models.DateTimeField(auto_now_add=True)
+    recurring = models.ForeignKey(RecurringExpense,null=True,blank=True,on_delete=models.SET_NULL,related_name='expenses')
 
 class Budget(models.Model):
     PERIOD_CHOICES = [
