e94d582ca64cf2be136c143683092a8e687fa285
ali balochi <alibalochi1910@gmail.com>
2025-09-04 15:29:26 +0330

    create serializer for recurring expenses

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Expense,Category
+from .models import Expense,Category,RecurringExpense
+from datetime import datetime
 
 class ExpensesSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,3 +27,42 @@ class CategorySerializer(serializers.ModelSerializer):
             setattr(instance,field,validated_data.get(field,getattr(instance,field)))
         instance.save()
         return instance
+
+class RecurringExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecurringExpense
+        fields = '__all__'
+        read_only_fields = ['user', 'created_at', 'updated_at']
+        extra_kwargs = {
+            'frequency': {'required': False},
+            'anchor_date': {'required': False},
+            'category': {'required': False},
+            'amount': {'required': False},
+            'next_run_at': {'required': False},
+            'end_date': {'required': False},
+            'description': {'required': False},
+        }
+
+    def validate(self, data):
+        user = self.context['request'].user
+
+        if 'amount' in data and data['amount'] <= 0:
+            raise serializers.ValidationError({"amount": "Amount must be positive."})
+
+        if 'anchor_date' in data and 'end_date' in data and data['end_date'] < data['anchor_date']:
+            raise serializers.ValidationError({"end_date": "End date cannot be before anchor date."})
+
+        if 'frequency' in data:
+            if data['frequency'] not in ['daily', 'weekly', 'monthly', 'yearly']:
+                raise serializers.ValidationError({
+                    "frequency": "Frequency must be one of these: daily, weekly, monthly, yearly."
+                })
+
+        if 'category' in data:
+            category = data['category']
+            if not (category.is_default or category.user == user):
+                raise serializers.ValidationError({
+                    "category": "The category is not valid or does not belong to you."
+                })
+
+        return data
