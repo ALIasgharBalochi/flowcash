1deb838048814c149e6ad8aba230c1eb3b636bce
ali balochi <alibalochi1910@gmail.com>
2025-09-04 15:31:20 +0330

    create url for crud actions

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ExpensesView,CategoryView,ExpenseDetailView,CategoryDetailView
+from .views import ExpensesView,CategoryView,ExpenseDetailView,CategoryDetailView,RecurringExpenseView,RecurringExpensesDetailView
 
 urlpatterns = [
     path('expenses/',ExpensesView.as_view(), name='expenses'),
     path('category/',CategoryView.as_view(), name='category'),
+    path('recurring_expenses/',RecurringExpenseView.as_view(),name='recurring_expenses'),
     path('expenses_details/<int:pk>/', ExpenseDetailView.as_view(),name='expenses_details'),
-    path('category_details/<int:pk>/', CategoryDetailView.as_view(), name='category_details')
+    path('category_details/<int:pk>/', CategoryDetailView.as_view(), name='category_details'),
+    path('recurring_expenses_details/<int:pk>/',RecurringExpensesDetailView.as_view(),name='recurring_expenses_details')
 ]
