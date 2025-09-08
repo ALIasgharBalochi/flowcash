33abed04c197d7ab5d4f4e6b5a5907395dd5a0dd
ali balochi <alibalochi1910@gmail.com>
2025-09-08 22:35:50 +0330

    create task for get recurring_expenses and create expenses by recurring_expenses

--- expenses/tasks.py
+++ expenses/tasks.py
@@ -1,7 +1,19 @@
 from celery import shared_task
-
-import time
+from .models import Expense,RecurringExpense
+from datetime import date,datetime
 
 @shared_task
-def test_task():
-    pass
+def creating_recurring_costs():
+        recurrings = RecurringExpense.objects.select_related('category','user').filter(next_run_at=date.today())   
+        if recurrings:
+            for recurring in recurrings:
+                Expense.objects.create(
+                    amount=recurring.amount,
+                    category=recurring.category,
+                    date=datetime.today(),
+                    description=recurring.description,
+                    user=recurring.user,
+                    )
+                recurring.next_run_at = recurring.get_next_run()
+                recurring.save()
+
