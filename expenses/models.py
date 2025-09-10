15558f55ba601dae7db76d37c6f2f9db3ff96540
ali balochi <alibalochi1910@gmail.com>
2025-09-10 17:29:18 +0330

    add active field to budget model

--- expenses/models.py
+++ expenses/models.py
@@ -55,8 +55,9 @@ class Budget(models.Model):
         ("yearly","Yearly")
     ]
     user = models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='budgets')
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='budgets')
+    category = models.ForeignKey(Category,null=True,blank=True,on_delete=models.SET_NULL,related_name='budgets')
     amount = models.DecimalField(max_digits=20,decimal_places=2)
     period = models.CharField(max_length=20,choices=PERIOD_CHOICES,default="monthly")
+    active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
