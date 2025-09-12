80983bf6b4e3c1fa1a70522b371e01028950b8c1
ali balochi <alibalochi1910@gmail.com>
2025-09-12 20:40:49 +0330

    Create calculate_expenses_sum to get the amount of expenses recorded in a specific date range

---
+++ expenses/services.py
@@ -0,0 +1,19 @@
+from .models import Expense
+from django.db.models import Sum
+
+def calculate_expenses_sum(user,start_date,end_date,category=None):
+    filters = {
+        'user': user,
+        'date__gte':start_date,
+        'date__lte':end_date,
+    }
+    if category:
+        filters['category'] = category
+
+    total_amount = Expense.objects.filter(**filters).aaggregate(
+        total=Sum('amount'
+                  ))['total'] or 0
+
+    return total_amount
+
+    
