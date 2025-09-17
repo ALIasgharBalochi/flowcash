dbdb64e4a6b1cdcf736e5fb8025eb7063ccf0dd2
ali balochi <alibalochi1910@gmail.com>
2025-09-17 21:11:31 +0330

    create url for get and create budget

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ExpensesView,CategoryView,ExpenseDetailView,CategoryDetailView,RecurringExpenseView,RecurringExpensesDetailView
+from .views import BudgetView,ExpensesView,CategoryView,ExpenseDetailView,CategoryDetailView,RecurringExpenseView,RecurringExpensesDetailView
 
 urlpatterns = [
     path('expenses/',ExpensesView.as_view(), name='expenses'),
     path('category/',CategoryView.as_view(), name='category'),
     path('recurring_expenses/',RecurringExpenseView.as_view(),name='recurring_expenses'),
+    path('budget/',BudgetView.as_view(),name='budget'),
     path('expenses_details/<uuid:uuid>/', ExpenseDetailView.as_view(),name='expenses_details'),
     path('category_details/<uuid:uuid>/', CategoryDetailView.as_view(), name='category_details'),
-    path('recurring_expenses_details/<uuid:uuid>/',RecurringExpensesDetailView.as_view(),name='recurring_expenses_details')
+    path('recurring_expenses_details/<uuid:uuid>/',RecurringExpensesDetailView.as_view(),name='recurring_expenses_details'),
 ]
