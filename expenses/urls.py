9a9c0b4062edede6e8e24b35748646adadbfa8d5
ali balochi <alibalochi1910@gmail.com>
2025-09-18 21:49:38 +0330

    create url for get status of budget

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BudgetDetails,BudgetView,ExpensesView,CategoryView,ExpenseDetailView,CategoryDetailView,RecurringExpenseView,RecurringExpensesDetailView
+from .views import BudgetStatusView,BudgetDetails,BudgetView,ExpensesView,CategoryView,ExpenseDetailView,CategoryDetailView,RecurringExpenseView,RecurringExpensesDetailView
 
 urlpatterns = [
     path('expenses/',ExpensesView.as_view(), name='expenses'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('expenses_details/<uuid:uuid>/', ExpenseDetailView.as_view(),name='expenses_details'),
     path('category_details/<uuid:uuid>/', CategoryDetailView.as_view(), name='category_details'),
     path('recurring_expenses_details/<uuid:uuid>/',RecurringExpensesDetailView.as_view(),name='recurring_expenses_details'),
-    path('budget_details/<uuid:uuid>/',BudgetDetails.as_view(),name='budget_details')
+    path('budget_details/<uuid:uuid>/',BudgetDetails.as_view(),name='budget_details'),
+    path('budget/<uuid:uuid>/status/',BudgetStatusView.as_view(),name='budget_status')
 ]
