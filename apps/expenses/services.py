09e56c62570d2618b9a241f63580faaf9aa9e0f3
ali balochi <alibalochi1910@gmail.com>
2025-09-24 09:07:04 +0330

    create calculate_budget_status in services

--- apps/expenses/services.py
+++ apps/expenses/services.py
@@ -1,5 +1,6 @@
 from .models import Expense
 from django.db.models import Sum
+from datetime import date
 
 def calculate_expenses_sum(user,start_date,end_date,category=None):
     filters = {
@@ -16,4 +17,9 @@ def calculate_expenses_sum(user,start_date,end_date,category=None):
 
     return total_amount
 
+def calculate_budget_status(budget,user):
+    amount_spent = calculate_expenses_sum(user,budget.created_at.date(),date.today(),budget.category)
+    remaining = budget.amount - amount_spent
+    percentage = (amount_spent / budget.amount * 100) if budget.amount > 0 else 0
+    return amount_spent,remaining,percentage
     
